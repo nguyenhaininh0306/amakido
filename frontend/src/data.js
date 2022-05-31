f65fbf823d49308475a23c1ee2ef185c59c2be1f
Hải Ninh Nguyễn <nninh4647@gmail.com>
2022-05-31 15:47:26 +0700

    react-router-dom

---
+++ frontend/src/data.js
@@ -0,0 +1,44 @@
+const data = {
+  product: [
+    {
+      name: 'Nike Slim shirt',
+      slug: 'nike-slim-shirt',
+      category: 'Shirts',
+      image: './images/p1.jpg',
+      price: 250,
+      countInStock: 20,
+      brand: 'Nike',
+      rating: 4.0,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+
+    {
+      name: 'Nike Slim T-shirt',
+      slug: 'nike-slim-T-shirt',
+      category: 'Shirts',
+      image: './images/p1.jpg',
+      price: 250,
+      countInStock: 20,
+      brand: 'Nike',
+      rating: 4.0,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+
+    {
+      name: 'Nike Slim pant',
+      slug: 'nike-slim-pant',
+      category: 'Pants',
+      image: './images/p1.jpg',
+      price: 250,
+      countInStock: 20,
+      brand: 'Nike',
+      rating: 4.0,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+  ],
+}
+
+export default data
