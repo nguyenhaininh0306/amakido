bf5a6b7cb8b8fc9d09c9c48712db6a72c2664c57
Hải Ninh Nguyễn <nninh4647@gmail.com>
2022-05-31 20:02:52 +0700

    add react bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,36 @@
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 import HomeScreen from './pages/HomeScreen'
 import ProductScreen from './pages/ProductScreen'
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import { LinkContainer } from 'react-router-bootstrap'
 
 function App() {
   return (
     <BrowserRouter>
-      <div className='App'>
+      <div className='d-flex flex-column site-container'>
         <header className='App-header'>
-          <Link to='/'>amakido</Link>
+          <Navbar bg='dark' variant='dark'>
+            <Container>
+              <LinkContainer to='/'>
+                <Navbar.Brand>amakido</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
+
         <main>
-          <Routes>
-            <Route path='/product/:slug' element={<ProductScreen />} />
-            <Route path='/' element={<HomeScreen />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path='/product/:slug' element={<ProductScreen />} />
+              <Route path='/' element={<HomeScreen />} />
+            </Routes>
+          </Container>
         </main>
+
+        <footer>
+          <div className='text-center'>All rights reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   )
--- frontend/src/pages/HomeScreen.js
+++ frontend/src/pages/HomeScreen.js
@@ -1,28 +1,93 @@
-import React from 'react'
+import React, { useEffect, useReducer, useState } from 'react'
 import { Link } from 'react-router-dom'
-import data from '../data'
+import axios from 'axios'
+import logger from 'use-reducer-logger'
+
+//create initialState
+const initialStateProduct = {
+  products: [],
+  loading: true,
+  error: '',
+}
+
+//create constant
+const FETCH_REQUEST = 'FETCH_REQUEST'
+const FETCH_SUCCESS = 'FETCH_SUCCESS'
+const FETCH_FAIL = 'FETCH_FAIL'
+
+//create reducer
+const reducer = (state, action) => {
+  switch (action.type) {
+    case FETCH_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      }
+    case FETCH_SUCCESS:
+      return {
+        ...state,
+        products: action.payload,
+        loading: false,
+      }
+    case FETCH_FAIL:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      }
+    default:
+      return state
+  }
+}
 
 const HomeScreen = () => {
+  const [{ loading, error, products }, dispatch] = useReducer(
+    logger(reducer),
+    initialStateProduct
+  )
+
+  //   const [products, setProducts] = useState([])
+
+  //fetch data từ server
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch(FETCH_REQUEST)
+      try {
+        const result = await axios.get('/api/products')
+        dispatch({ type: FETCH_SUCCESS, payload: result.data })
+      } catch (error) {
+        dispatch({ type: FETCH_FAIL, payload: error.message })
+      }
+    }
+    fetchData()
+  }, [])
+
   return (
     <div>
       <h1>Featured Products</h1>
       <div className='products'>
-        {data.product.map((product) => (
-          <div className='product' key={product.slug}>
-            <Link to={`/product/${product.slug}`}>
-              <img src={product.image} alt={product.name} />
-            </Link>
-            <div className='product-info'>
+        {loading ? (
+          <div>Loading...</div>
+        ) : error ? (
+          <div>{error}</div>
+        ) : (
+          products.map((product) => (
+            <div className='product' key={product.slug}>
               <Link to={`/product/${product.slug}`}>
-                <p>
-                  <strong>{product.name}</strong>
-                </p>
+                <img src={product.image} alt={product.name} />
               </Link>
-              <p>{product.price}</p>
-              <button>Add to Cart</button>
+              <div className='product-info'>
+                <Link to={`/product/${product.slug}`}>
+                  <p>
+                    <strong>{product.name}</strong>
+                  </p>
+                </Link>
+                <p>{product.price}</p>
+                <button>Add to Cart</button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </div>
   )
