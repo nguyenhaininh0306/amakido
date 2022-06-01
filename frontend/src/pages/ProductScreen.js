73cdaa0e467f8aaa04c0f06eb64e7e4f1a595491
Hải Ninh Nguyễn <nninh4647@gmail.com>
2022-06-01 09:36:37 +0700

    create Context

--- frontend/src/pages/ProductScreen.js
+++ frontend/src/pages/ProductScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useReducer } from 'react'
+import React, { useContext, useEffect, useReducer } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
 import Row from 'react-bootstrap/Row'
@@ -11,6 +11,7 @@ import Rating from '../components/Rating'
 import LoadingBox from '../components/LoadingBox'
 import MessageBox from '../components/MessageBox'
 import { getError } from '../utils'
+import { Store } from '../Store'
 
 //create initialState
 const initialStateProduct = {
@@ -71,6 +72,11 @@ const ProductScreen = () => {
     fetchData()
   }, [slug])
 
+  const { state, dispatch: ctxDispatch } = useContext(Store)
+  const addToCartHandler = () => {
+    ctxDispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity: 1 } })
+  }
+
   return loading ? (
     <LoadingBox />
   ) : error ? (
@@ -128,7 +134,9 @@ const ProductScreen = () => {
                 {product.countInStock > 0 && (
                   <ListGroup.Item>
                     <div className='d-grid'>
-                      <Button variant='primary'>Add to Card</Button>
+                      <Button onClick={addToCartHandler} variant='primary'>
+                        Add to Card
+                      </Button>
                     </div>
                   </ListGroup.Item>
                 )}
